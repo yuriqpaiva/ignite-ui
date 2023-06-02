013db2a49ea6e71bb99c371dcced95b42f54a531
Yuri Paiva <yuriqpaiva@gmail.com>
2023-06-02 12:17:03 -0300

    Setup storybook

---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,32 @@
+import {themes} from '@storybook/theming'
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#F8F8F8',
+        },
+        {
+          name: 'dark',
+          value: '#333333',
+        },
+      ],
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      theme: themes.dark,
+    }
+  },
+};
+
+export default preview;
