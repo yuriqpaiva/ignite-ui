8c7873fe003302dc8ee206fedc8ea1d80aa13c6a
Yuri Paiva <yuriqpaiva@gmail.com>
2023-06-03 14:24:58 -0300

    Add button action log

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -9,6 +9,9 @@ export default {
   args: {
     children: 'Send',
   },
+  argTypes: {
+    onClick: { action: 'click' },
+  },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
